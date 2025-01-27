21eba8633070f5977b1d528312856a1321059487
Egor Vasyakov <waccess.vasyakov@crpt.ru>
2025-01-27 23:31:35 +0300

    Create backend skeleton

---
+++ my-renovation-service/src/test/java/ru/veqveq/renovation/MyRenovationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veqveq.renovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRenovationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
